70203efecceb335bcf93858019e2370b8cc95f03
me-shahbazi <me_shahbazi@outlook.com>
2025-02-05 23:40:05 +0330

    Almost Done

--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -87,7 +87,7 @@ contract lottery is  VRFV2PlusWrapperConsumerBase, ConfirmedOwner {
     }
     
     function randomNumCalc() public view onlyOwner returns(uint) { // Do NOT forget {"from": ownerAccount} when ever you gonna call this func using brownie
-        uint rand = uint256(keccak256(abi.encodePacked(block.number,blockhash(block.number-5), block.timestamp, block.difficulty, /*block.prevrandao,*/ msg.data))) % 100;
+        uint rand = uint256(keccak256(abi.encodePacked(block.number,blockhash(block.number-5), block.timestamp, /*block.difficulty, /*block.prevrandao,*/ msg.data))) % 100;
         return rand;
     }
 
